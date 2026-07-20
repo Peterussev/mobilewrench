5a793671ce4afdac962a698782072ead60fa1791
Peterussev <46545018+Peterussev@users.noreply.github.com>
2026-07-20 16:48:10 +0000

    Restore MobileWrench React project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <h1>🔧 MobileWrench</h1>
+      <h2>Find a mechanic when you need one</h2>
+      <p>Prototype is back online!</p>
+
+      <button>
+        Customer Request
+      </button>
+    </div>
+  );
+}
+
+export default App;
